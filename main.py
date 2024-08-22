240be247fa472d5b7c7f5d0ff7351981aa1d9254
lmanan <manan.lalit@gmail.com>
2024-08-22 11:20:18 -0400

    Consolidate main.py and main_skipeedge.py.

--- main.py
+++ main.py
@@ -1,31 +1,102 @@
 import tifffile
 import numpy as np
 from natsort import natsorted
-import argparse
+from typing import cast
+import jsonargparse
 from motile_toolbox.candidate_graph import (
     get_candidate_graph,
     nodes_from_segmentation,
     NodeAttr,
     graph_to_nx,
 )
+from motile.variables import EdgeSelected
 from motile.track_graph import TrackGraph
 from motile.solver import Solver
 from motile.constraints import MaxParents, MaxChildren
-from motile.costs import Appear, Disappear, Split, EdgeDistance
+from motile.costs import Costs, Weight, Appear, Disappear, Split, EdgeDistance
 import logging
-from saving_utils import save_result_tifs_res_track
 from pathlib import Path
-from run_traccuracy import compute_metrics
-from division_costs import NormalEdgeDistance, HyperEdgeDistance
+from division_costs import (
+    NormalEdgeDistance,
+    HyperEdgeDistance,
+)
 from itertools import combinations
-import sys
-from datetime import date
-
+import json
+import networkx as nx
+from run_traccuracy_skip_edge import compute_metrics
+from saving_utils_skip_edge import save_result_tifs_json
+import os
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s %(name)s %(levelname)-8s %(message)s"
 )
 logger = logging.getLogger(__name__)
+
+
+class NormalTimeGap(Costs):
+    def __init__(
+        self,
+        time_attribute: str | tuple[str, ...],
+        weight: float = 1.0,
+        constant: float = 0.0,
+    ) -> None:
+        self.time_attribute = time_attribute
+        self.weight = Weight(weight)
+        self.constant = Weight(constant)
+
+    def apply(self, solver: Solver) -> None:
+        edge_variables = solver.get_variables(EdgeSelected)
+        for key, index in edge_variables.items():
+            if type(key[1]) is tuple:  # hyper edge
+                solver.add_variable_cost(index, 0.0, self.weight)
+                solver.add_variable_cost(index, 0.0, self.constant)
+            else:
+                u, v = cast("tuple[int, int]", key)
+                time_u = self.__get_node_frame(solver.graph, u)
+                time_v = self.__get_node_frame(solver.graph, v)
+                feature = np.linalg.norm(time_u - time_v)
+                solver.add_variable_cost(index, feature, self.weight)
+                solver.add_variable_cost(index, 1.0, self.constant)
+
+    def __get_node_frame(self, graph: nx.DiGraph, node: int) -> np.ndarray:
+        if isinstance(self.time_attribute, tuple):
+            return np.array([graph.nodes[node][p] for p in self.time_attribute])
+        else:
+            return np.array(graph.nodes[node][self.time_attribute])
+
+
+class HyperTimeGap(Costs):
+    def __init__(
+        self,
+        time_attribute: str | tuple[str, ...],
+        weight: float = 1.0,
+        constant: float = 0.0,
+    ) -> None:
+        self.time_attribute = time_attribute
+        self.weight = Weight(weight)
+        self.constant = Weight(constant)
+
+    def apply(self, solver: Solver) -> None:
+        edge_variables = solver.get_variables(EdgeSelected)
+        for key, index in edge_variables.items():
+            if type(key[1]) is tuple:
+                (start,) = key[0]
+                end1, end2 = key[1]
+                time_start = self.__get_node_frame(solver.graph, start)
+                time_end1 = self.__get_node_frame(solver.graph, end1)
+                time_end2 = self.__get_node_frame(solver.graph, end2)
+                feature = np.linalg.norm(time_start - 0.5 * (time_end1 + time_end2))
+                solver.add_variable_cost(index, feature, self.weight)
+                solver.add_variable_cost(index, 1.0, self.constant)
+            else:
+                solver.add_variable_cost(index, 0.0, self.weight)
+                solver.add_variable_cost(index, 0.0, self.constant)
+
+    def __get_node_frame(self, graph: nx.DiGraph, node: int) -> np.ndarray:
+        if isinstance(self.time_attribute, tuple):
+            return np.array([graph.nodes[node][p] for p in self.time_attribute])
+        else:
+            return np.array(graph.nodes[node][self.time_attribute])
 
 
 def add_hyper_elements(candidate_graph):
@@ -34,19 +105,15 @@ def add_hyper_elements(candidate_graph):
         out_edges = candidate_graph.out_edges(node)
         pairs = list(combinations(out_edges, 2))
         for pair in pairs:
-            candidate_graph.add_node(
-                str(pair[0][0]) + "_" + str(pair[0][1]) + "_" + str(pair[1][1])
-            )
+            temp_node = str(pair[0][0]) + "_" + str(pair[0][1]) + "_" + str(pair[1][1])
+            candidate_graph.add_node(temp_node)
+            candidate_graph.add_edge(pair[0][0], temp_node)
             candidate_graph.add_edge(
-                pair[0][0],
-                str(pair[0][0] + "_" + str(pair[0][1]) + "_" + str(pair[1][1])),
-            )
-            candidate_graph.add_edge(
-                str(pair[0][0]) + "_" + str(pair[0][1]) + "_" + str(pair[1][1]),
+                temp_node,
                 pair[0][1],
             )
             candidate_graph.add_edge(
-                str(pair[0][0]) + "_" + str(pair[0][1]) + "_" + str(pair[1][1]),
+                temp_node,
                 pair[1][1],
             )
     return candidate_graph
@@ -74,12 +141,26 @@ def add_costs_and_constraints(solver, symmetric_division_cost):
             ),
             name="Hyper Edge Distance",
         )
+
+        solver.add_costs(
+            NormalTimeGap(weight=1.0, constant=0.0, time_attribute=NodeAttr.TIME.value),
+            name="Normal Time Gap",
+        )
+
+        solver.add_costs(
+            HyperTimeGap(weight=1.0, constant=0.0, time_attribute=NodeAttr.TIME.value),
+            name="Hyper Time Gap",
+        )
     else:
         solver.add_costs(
             EdgeDistance(
                 weight=1.0, constant=-20.0, position_attribute=NodeAttr.POS.value
             ),
             name="Position",
+        )
+        solver.add_costs(
+            NormalTimeGap(weight=1.0, constant=0.0, time_attribute=NodeAttr.TIME.value),
+            name="Normal Time Gap",
         )
         solver.add_costs(Split(constant=0.5), name="Division")
 
@@ -94,6 +175,8 @@ def track(
     max_edge_distance: float,
     regularizer_weight: float,
     symmetric_division_cost: bool,
+    dT: int,
+    results_dir: str,
 ):
     """
     This function does four things:
@@ -137,15 +220,14 @@ def track(
         Additionally, the cost for division is different.
     """
 
-    today = date.today()
-    sys.stdout = open(val_segmentation_dir_name + "_" + str(today), "w")
     # Step 1 ------------------------------
 
     # obtain train masks
     train_filenames = natsorted(list(Path(train_segmentation_dir_name).glob("*.tif")))
     train_segmentation = []
     for train_filename in train_filenames:
-        train_segmentation.append(tifffile.imread(train_filename))
+        ma = tifffile.imread(train_filename)
+        train_segmentation.append(ma)
     train_segmentation = np.asarray(train_segmentation)
     train_segmentation = train_segmentation[
         :, np.newaxis
@@ -155,32 +237,43 @@ def track(
     val_filenames = natsorted(list(Path(val_segmentation_dir_name).glob("*.tif")))
     val_segmentation = []
     for val_filename in val_filenames:
-        val_segmentation.append(tifffile.imread(val_filename))
+        ma = tifffile.imread(val_filename)
+        val_segmentation.append(ma)
     val_segmentation = np.asarray(val_segmentation)
     val_segmentation = val_segmentation[:, np.newaxis]  # requires a hypothesis channel
 
     # get train candidate graph
     train_candidate_graph, _ = get_candidate_graph(
-        segmentation=train_segmentation, max_edge_distance=max_edge_distance, iou=False
+        segmentation=train_segmentation,
+        max_edge_distance=max_edge_distance,
+        iou=False,
+        dT=dT,
     )
+
     if symmetric_division_cost:
         print(" Adding hyper elements to train candidate graph ...")
         train_candidate_graph = add_hyper_elements(train_candidate_graph)
 
     # get val candidate graph
     val_candidate_graph, _ = get_candidate_graph(
-        segmentation=val_segmentation, max_edge_distance=max_edge_distance, iou=False
+        segmentation=val_segmentation,
+        max_edge_distance=max_edge_distance,
+        iou=False,
+        dT=dT,
     )
 
     if symmetric_division_cost:
         print(" Adding hyper elements to val candidate graph ...")
         val_candidate_graph = add_hyper_elements(val_candidate_graph)
 
+    print("Converting train candidate graph to train track graph")
+
     # build a train track graph
     train_track_graph = TrackGraph(
         nx_graph=train_candidate_graph, frame_attribute="time"
     )
 
+    print("Converting val candidate graph to val track graph")
     # build a val track graph
     val_track_graph = TrackGraph(nx_graph=val_candidate_graph, frame_attribute="time")
 
@@ -192,62 +285,57 @@ def track(
     groundtruth_graph, node_frame_dict = nodes_from_segmentation(train_segmentation)
 
     # add edges
-    gt_data = np.loadtxt(
-        Path(train_segmentation_dir_name).joinpath("man_track.txt"), delimiter=" "
-    )
+    f = open(train_segmentation_dir_name + "/man_track.json")
+    gt_data = json.load(f)
+
     parent_daughter_dict = {}
-    gt_dict = {}
 
-    for row in gt_data:
-        id_, t_st, t_end, parent_id = int(row[0]), int(row[1]), int(row[2]), int(row[3])
-        gt_dict[id_] = [t_st, t_end]
-
-    for row in gt_data:
-        id_, t_st, t_end, parent_id = int(row[0]), int(row[1]), int(row[2]), int(row[3])
+    for key, value in gt_data.items():
+        parent_id = int(value[1])
         if parent_id in parent_daughter_dict.keys():
             pass
         else:
             parent_daughter_dict[parent_id] = []
-        parent_daughter_dict[parent_id].append(id_)
+        parent_daughter_dict[parent_id].append(
+            str(int(np.min(value[0]))) + "_" + str(int(key))
+        )
+        # t_id ...
 
-    for row in gt_data:
-        id_, t_st, t_end, parent_id = int(row[0]), int(row[1]), int(row[2]), int(row[3])
-
-        for t in range(t_st, t_end):
+    for key, value in gt_data.items():
+        frames, parent_id = value
+        for index in range(len(frames) - 1):
             groundtruth_graph.add_edge(
-                str(t) + "_" + str(id_), str(t + 1) + "_" + str(id_)
-            )
-
+                str(frames[index]) + "_" + str(key),
+                str(frames[index + 1]) + "_" + str(key),
+            )  # intra tracklet
         if parent_id != 0:
-            time_parent = gt_dict[parent_id][1]
-            temp_node = str(time_parent) + "_" + str(parent_id)
-            for daughter_index in range(len(parent_daughter_dict[parent_id])):
-                temp_node += (
-                    "_"
-                    + str(t_st)
-                    + "_"
-                    + str(parent_daughter_dict[parent_id][daughter_index])
-                )
+            time_parent = int(
+                np.max(gt_data[str(parent_id)][0])
+            )  # last time where parent showed up
+            parent_node = str(time_parent) + "_" + str(parent_id)
+
+            temp_node = parent_node
+            for daughter_node in parent_daughter_dict[parent_id]:
+                temp_node += "_" + daughter_node
+
             if symmetric_division_cost:
-                groundtruth_graph.add_node(temp_node)
-                groundtruth_graph.add_edge(
-                    str(gt_dict[parent_id][1]) + "_" + str(parent_id), temp_node
-                )
-                for daughter_index in range(len(parent_daughter_dict[parent_id])):
+                if len(parent_daughter_dict[parent_id]) == 1:
                     groundtruth_graph.add_edge(
-                        temp_node,
-                        str(t_st)
-                        + "_"
-                        + str(parent_daughter_dict[parent_id][daughter_index]),
+                        parent_node, parent_daughter_dict[parent_id][0]
                     )
+                elif len(parent_daughter_dict[parent_id]) == 2:
+                    groundtruth_graph.add_node(temp_node)
+                    groundtruth_graph.add_edge(parent_node, temp_node)
+                    for daughter_node in parent_daughter_dict[parent_id]:
+                        groundtruth_graph.add_edge(temp_node, daughter_node)
             else:
                 groundtruth_graph.add_edge(
-                    str(gt_dict[parent_id][1]) + "_" + str(parent_id),
-                    str(t_st) + "_" + str(id_),
+                    parent_node, str(int(np.min(value[0]))) + "_" + str(key)
                 )
 
-    # build a groundtruth track graph
+    ## build a groundtruth track graph
 
+    print("Converting train gt graph to train track graph")
     groundtruth_track_graph = TrackGraph(
         nx_graph=groundtruth_graph, frame_attribute="time"
     )
@@ -257,14 +345,12 @@ def track(
     ## fitting weights now ...
 
     np.random.seed(42)
-
     for node_id in train_track_graph.nodes:
         for edge_id in list(train_track_graph.next_edges[node_id]):
             if edge_id in groundtruth_track_graph.edges:
                 train_track_graph.edges[edge_id]["gt"] = True
             else:
                 train_track_graph.edges[edge_id]["gt"] = False
-
     # fit weights
     solver = Solver(track_graph=train_track_graph)
     solver = add_costs_and_constraints(solver, symmetric_division_cost)
@@ -272,59 +358,91 @@ def track(
         gt_attribute="gt", regularizer_weight=regularizer_weight, max_iterations=1000
     )
     optimal_weights = solver.weights
+    weights_by_name = optimal_weights._weights_by_name
     print(f"After fitting, optimal weights are {optimal_weights}")
+    optimal_weights_array = optimal_weights.to_ndarray()
+
+    fitted_weights_dic = {}
+    for i, key in enumerate(weights_by_name):
+        fitted_weights_dic[key[0] + "_" + key[1]] = str(optimal_weights_array[i])
+
+    with open(results_dir + "/weights.json", "w") as fp:
+        json.dump(fitted_weights_dic, fp)
 
     # Step 4 ---------------------------------
 
     solver = Solver(track_graph=val_track_graph)
     solver = add_costs_and_constraints(solver, symmetric_division_cost)
-    solver.weights.from_ndarray(optimal_weights.to_ndarray())
-
-    # save weights to a json
+    solver.weights.from_ndarray(optimal_weights_array)
 
     solution = solver.solve(verbose=True)
     solution_graph = solver.get_selected_subgraph(solution)
 
-    new_mapping, res_track, new_segmentations = save_result_tifs_res_track(
-        graph_to_nx(solution_graph),
-        val_segmentation[:, 0],
-        output_tif_dir="01_RES_SSVM",
+    new_mapping, res_track, tracked_masks = save_result_tifs_json(
+        solution_nx_graph=graph_to_nx(solution_graph),
+        segmentation=val_segmentation[:, 0],
+        output_tif_dir=results_dir,
     )
+
     print("Computing scores ...")
-    compute_metrics(val_segmentation_dir_name=val_segmentation_dir_name)
-    sys.stdout.close()
+    compute_metrics(
+        gt_json_file_name=val_segmentation_dir_name + "/man_track.json",
+        gt_segmentation=val_segmentation[:, 0],
+        pred_json_file_name=results_dir + "/man_track.json",
+        pred_segmentation=tracked_masks,
+        results_dir=results_dir,
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = jsonargparse.ArgumentParser()
     parser.add_argument(
         "--train_segmentation_dir_name",
         dest="train_segmentation_dir_name",
-        default="./Fluo-N2DL-HeLa/02_GT/TRA/",
+        default="./Fluo-N2DL-HeLa/02_GT0.0/",
     )
     parser.add_argument(
         "--val_segmentation_dir_name",
         dest="val_segmentation_dir_name",
-        default="./Fluo-N2DL-HeLa/01_GT/TRA/",
+        default="./Fluo-N2DL-HeLa/01_GT0.0/",
     )
-    parser.add_argument("--max_edge_distance", dest="max_edge_distance", default=50)
+    parser.add_argument(
+        "--max_edge_distance", dest="max_edge_distance", type=float, default=50
+    )
     parser.add_argument(
         "--regularizer_weight", dest="regularizer_weight", default=1e2, type=float
     )
     parser.add_argument(
         "--symmetric_division_cost",
         dest="symmetric_division_cost",
-        action=argparse.BooleanOptionalAction,
+        type=bool,
         default=False,
     )
+    parser.add_argument(
+        "--results_dir_name", dest="results_dir_name", default="Fluo-N2DL-HeLa_results/"
+    )
+
+    parser.add_argument("--dT", dest="dT", type=int, default=1)
     args = parser.parse_args()
     print("+" * 10)
     print(args)
     print("+" * 10)
+
+    if os.path.exists(args.results_dir_name):
+        pass
+    else:
+        os.makedirs(args.results_dir_name)
+
+    parser.save(
+        args, args.results_dir_name + "/args.json", format="json", overwrite=True
+    )
+
     track(
         train_segmentation_dir_name=args.train_segmentation_dir_name,
         val_segmentation_dir_name=args.val_segmentation_dir_name,
         max_edge_distance=args.max_edge_distance,
         regularizer_weight=args.regularizer_weight,
         symmetric_division_cost=args.symmetric_division_cost,
+        dT=args.dT,
+        results_dir=args.results_dir_name,
     )
