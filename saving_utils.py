3fb6c795d84f7f6475519492a95477c504bbb625
lmanan <manan.lalit@gmail.com>
2024-09-14 20:32:15 -0400

    Update `saving_utils.py`.

--- saving_utils.py
+++ saving_utils.py
@@ -4,13 +4,17 @@ import tifffile
 import numpy as np
 import networkx as nx
 import json
+from motile_toolbox.candidate_graph import NodeAttr
+from utils import expand_position
 
 
 def save_result(
-    solution_nx_graph: nx.DiGraph, segmentation: np.ndarray, output_tif_dir_name: str
+    solution_nx_graph: nx.DiGraph,
+    segmentation: np.ndarray,
+    output_tif_dir_name: str,
+    write_tifs: bool,
 ):
-
-    tracked_masks = np.zeros_like(segmentation)
+    tracked_masks = np.zeros(segmentation.shape, dtype=np.uint16)
     new_mapping = {}  # <t_id> in segmentation mask: id in tracking mask
     res_track = (
         {}
@@ -29,18 +33,55 @@ def save_result(
                     res_track[new_mapping[in_node]][0].append(
                         t_out
                     )  # include the end time for this tracklet
-                tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
+                position_in_node = solution_nx_graph.nodes[in_node][NodeAttr.POS.value]
+
+                tracked_masks[t_in] = expand_position(
+                    data=tracked_masks[t_in],
+                    position=position_in_node,
+                    id_=new_mapping[in_node],
+                )
+                # tracked_masks[t_in][tuple(position_in_node)] = new_mapping[in_node]
+                # tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
                 new_mapping[out_node] = new_mapping[in_node]
-                tracked_masks[t_out][segmentation[t_out] == id_out] = new_mapping[
-                    out_node
+
+                position_out_node = solution_nx_graph.nodes[out_node][
+                    NodeAttr.POS.value
                 ]
+
+                tracked_masks[t_out] = expand_position(
+                    data=tracked_masks[t_out],
+                    position=position_out_node,
+                    id_=new_mapping[out_node],
+                )
+
+                # tracked_masks[t_out][tuple(position_out_node)] = new_mapping[out_node]
+                # tracked_masks[t_out][segmentation[t_out] == id_out] = new_mapping[
+                #    out_node
+                # ]
             else:
                 # i.e. start of a new edge
                 res_track[id_counter] = ([t_in, t_out], 0)
                 new_mapping[in_node] = id_counter
                 new_mapping[out_node] = id_counter
-                tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
-                tracked_masks[t_out][segmentation[t_out] == id_out] = id_counter
+
+                position_in_node = solution_nx_graph.nodes[in_node][NodeAttr.POS.value]
+                position_out_node = solution_nx_graph.nodes[out_node][
+                    NodeAttr.POS.value
+                ]
+
+                tracked_masks[t_in] = expand_position(
+                    data=tracked_masks[t_in], position=position_in_node, id_=id_counter
+                )
+                # tracked_masks[t_in][tuple(position_in_node)] = id_counter
+
+                tracked_masks[t_out] = expand_position(
+                    data=tracked_masks[t_out],
+                    position=position_out_node,
+                    id_=id_counter,
+                )
+                # tracked_masks[t_out][tuple(position_out_node)] = id_counter
+                # tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
+                # tracked_masks[t_out][segmentation[t_out] == id_out] = id_counter
                 id_counter += 1
         elif num_out_edges == 2:
             out_edge1, out_edge2 = solution_nx_graph.out_edges(in_node)
@@ -54,30 +95,91 @@ def save_result(
                 # i.e. in node was connected by one outgoing edge previously
                 if t_in not in res_track[new_mapping[in_node]][0]:
                     res_track[new_mapping[in_node]][0].append(t_in)
-                tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
+
+                position_in_node = solution_nx_graph.nodes[in_node][NodeAttr.POS.value]
+
+                tracked_masks[t_in] = expand_position(
+                    data=tracked_masks[t_in],
+                    position=position_in_node,
+                    id_=new_mapping[in_node],
+                )
+
+                # tracked_masks[t_in][tuple(position_in_node)] = new_mapping[in_node]
+                # tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
                 if out_node1 not in new_mapping:
                     new_mapping[out_node1] = id_counter
-                    tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
+                    position_out_node1 = solution_nx_graph.nodes[out_node1][
+                        NodeAttr.POS.value
+                    ]
+
+                    tracked_masks[t_out1] = expand_position(
+                        data=tracked_masks[t_out1],
+                        position=position_out_node1,
+                        id_=id_counter,
+                    )
+
+                    # tracked_masks[t_out1][tuple(position_out_node1)] = id_counter
+                    # tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
                     res_track[id_counter] = ([t_out1], new_mapping[in_node])
                     id_counter += 1
                 if out_node2 not in new_mapping:
                     new_mapping[out_node2] = id_counter
-                    tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
+                    position_out_node2 = solution_nx_graph.nodes[out_node2][
+                        NodeAttr.POS.value
+                    ]
+
+                    tracked_masks[t_out2] = expand_position(
+                        data=tracked_masks[t_out2],
+                        position=position_out_node2,
+                        id_=id_counter,
+                    )
+                    # tracked_masks[t_out2][tuple(position_out_node2)] = id_counter
+                    # tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
                     res_track[id_counter] = ([t_out2], new_mapping[in_node])
                     id_counter += 1
             else:
                 res_track[id_counter] = ([t_in], 0)
                 new_mapping[in_node] = id_counter
-                tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
+
+                position_in_node = solution_nx_graph.nodes[in_node][NodeAttr.POS.value]
+
+                tracked_masks[t_in] = expand_position(
+                    data=tracked_masks[t_in], position=position_in_node, id_=id_counter
+                )
+
+                # tracked_masks[t_in][tuple(position_in_node)] = id_counter
+                # tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
                 id_counter += 1
                 if out_node1 not in new_mapping:
                     new_mapping[out_node1] = id_counter
-                    tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
+                    position_out_node1 = solution_nx_graph.nodes[out_node1][
+                        NodeAttr.POS.value
+                    ]
+
+                    tracked_masks[t_out1] = expand_position(
+                        data=tracked_masks[t_out1],
+                        position=position_out_node1,
+                        id_=id_counter,
+                    )
+
+                    # tracked_masks[t_out1][tuple(position_out_node1)] = id_counter
+                    # tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
                     res_track[id_counter] = ([t_out1], new_mapping[in_node])
                     id_counter += 1
                 if out_node2 not in new_mapping:
                     new_mapping[out_node2] = id_counter
-                    tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
+                    position_out_node2 = solution_nx_graph.nodes[out_node2][
+                        NodeAttr.POS.value
+                    ]
+
+                    tracked_masks[t_out2] = expand_position(
+                        data=tracked_masks[t_out2],
+                        position=position_out_node2,
+                        id_=id_counter,
+                    )
+
+                    # tracked_masks[t_out2][tuple(position_out_node2)] = id_counter
+                    # tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
                     res_track[id_counter] = ([t_out2], new_mapping[in_node])
                     id_counter += 1
 
@@ -91,8 +193,13 @@ def save_result(
         else:
             res_track[id_counter] = ([t], 0)
             new_mapping[node] = id_counter
+            position_node = solution_nx_graph.nodes[node][NodeAttr.POS.value]
+            tracked_masks[t] = expand_position(
+                data=tracked_masks[t], position=position_node, id_=id_counter
+            )
 
-            tracked_masks[t][segmentation[t] == id_node] = id_counter
+            # tracked_masks[t][tuple(position_node)] = id_counter
+            # tracked_masks[t][segmentation[t] == id_node] = id_counter
             id_counter += 1
 
     # ensure that path where tifs will be saved, exists.
@@ -102,14 +209,34 @@ def save_result(
             Path(filename).unlink()
     else:
         Path(output_tif_dir_name).mkdir()
+
     # write tifs
-    for i in range(tracked_masks.shape[0]):
-        tifffile.imwrite(
-            Path(output_tif_dir_name) / ("mask" + str(i).zfill(3) + ".tif"),
-            tracked_masks[i].astype(np.uint16),
-        )
+    if write_tifs:
+        for i in range(tracked_masks.shape[0]):
+            tifffile.imwrite(
+                Path(output_tif_dir_name) / ("mask" + str(i).zfill(3) + ".tif"),
+                tracked_masks[i].astype(np.uint16),
+            )
     # write man_track.json
     with open(output_tif_dir_name + "/jsons/res_track.json", "w") as f:
         json.dump(res_track, f)
 
-    return new_mapping, res_track, tracked_masks
+    print(f"Final id counter is {id_counter}")
+
+    G = nx.DiGraph()
+    for k, v in new_mapping.items():
+        t, id_ = k.split("_")
+        t, id_ = int(t), int(id_)
+        pos = solution_nx_graph.nodes[k]["pos"]
+        if len(pos) == 2:
+            y, x = pos
+            G.add_node(k, seg_id=v, time=t, y=y, x=x)
+        elif len(pos) == 3:
+            z, y, x = pos
+            G.add_node(k, seg_id=v, time=t, z=z, y=y, x=x)
+            print(f"node {k} mapped to {v} at pos {pos} and time {t}")
+    for edge in solution_nx_graph.edges:
+        u, v = edge
+        G.add_edge(u, v)
+
+    return new_mapping, res_track, tracked_masks, G
