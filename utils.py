ebbec4ecb5c5ac1bd0cac482440012a1822f408e
lmanan <manan.lalit@gmail.com>
2024-09-14 20:31:37 -0400

    Add `expand_position`.

--- utils.py
+++ utils.py
@@ -8,16 +8,17 @@ import json
 import motile
 from motile.constraints import MaxParents, MaxChildren
 from motile.costs import Appear, Disappear
-from new_costs import (
+from costs import (
     EdgeDistance,
     NodeEmbeddingDistance,
     EdgeEmbeddingDistance,
     TimeGap,
 )
 from motile_toolbox.candidate_graph import NodeAttr
+from typing import List
 
 
-def load_data(segmentation_dir_name: str, add_channel_axis=True):
+def load_tif_data(segmentation_dir_name: str, add_channel_axis=True):
     # obtain masks
     filenames = natsorted(list(Path(segmentation_dir_name).glob("*.tif")))
     segmentation = []
@@ -30,26 +31,36 @@ def load_data(segmentation_dir_name: str, add_channel_axis=True):
     return segmentation
 
 
+def load_csv_data(csv_file_name: str, delimiter=" "):
+    """Assuming ids are seg_id t z y x p_id"""
+    data = np.genfromtxt(fname=csv_file_name, delimiter=delimiter)
+
+    return data
+
+
 def add_hyper_edges(candidate_graph: nx.DiGraph):
-    nodes_original = list(candidate_graph.nodes)
+    candidate_graph_copy = nx.DiGraph()
+    candidate_graph_copy.add_nodes_from(candidate_graph.nodes(data=True))
+    candidate_graph_copy.add_edges_from(candidate_graph.edges(data=True))
+    nodes_original = list(candidate_graph_copy.nodes)
     for node in nodes_original:
-        out_edges = candidate_graph.out_edges(node)
+        out_edges = candidate_graph_copy.out_edges(node)
         pairs = list(combinations(out_edges, 2))
         for pair in pairs:
             temporary_node = (
                 str(pair[0][0]) + "_" + str(pair[0][1]) + "_" + str(pair[1][1])
             )
-            candidate_graph.add_node(temporary_node)
-            candidate_graph.add_edge(pair[0][0], temporary_node)
-            candidate_graph.add_edge(
+            candidate_graph_copy.add_node(temporary_node)
+            candidate_graph_copy.add_edge(pair[0][0], temporary_node)
+            candidate_graph_copy.add_edge(
                 temporary_node,
                 pair[0][1],
             )
-            candidate_graph.add_edge(
+            candidate_graph_copy.add_edge(
                 temporary_node,
                 pair[1][1],
             )
-    return candidate_graph
+    return candidate_graph_copy
 
 
 def flip_edges(candidate_graph: nx.DiGraph):
@@ -105,6 +116,39 @@ def add_gt_edges_to_graph(groundtruth_graph: nx.DiGraph, json_file_name: str):
     return groundtruth_graph
 
 
+def add_gt_edges_to_graph_2(groundtruth_graph: nx.DiGraph, gt_data: np.ndarray):
+    """gt data will have last column as parent id column."""
+    parent_daughter_dictionary = {}
+    for row in gt_data:
+        id_, t, parent_id = int(row[0]), int(row[1]), int(row[-1])
+        if row[-1] == -1:
+            pass
+        else:
+            if parent_id in parent_daughter_dictionary:
+                parent_daughter_dictionary[parent_id].append(id_)
+            else:
+                parent_daughter_dictionary[parent_id] = [id_]
+
+    for row in gt_data:
+        id_, t, parent_id = int(row[0]), int(row[1]), int(row[-1])
+        if parent_id != -1:
+            if len(parent_daughter_dictionary[parent_id]) == 1:
+                start_node = str(t - 1) + "_" + str(parent_id)
+                end_node = str(t) + "_" + str(id_)
+                groundtruth_graph.add_edge(start_node, end_node)
+            elif len(parent_daughter_dictionary[parent_id]) == 2:
+                start_node = str(t - 1) + "_" + str(parent_id)
+                temporary_node = start_node
+                for daughter_node in parent_daughter_dictionary[parent_id]:
+                    temporary_node += "_" + str(t) + "_" + str(daughter_node)
+                groundtruth_graph.add_node(temporary_node)
+                groundtruth_graph.add_edge(start_node, temporary_node)
+                for daughter_node in parent_daughter_dictionary[parent_id]:
+                    end_node = str(t) + "_" + str(daughter_node)
+                    groundtruth_graph.add_edge(temporary_node, end_node)
+    return groundtruth_graph
+
+
 def add_costs(
     solver: motile.Solver,
     dT: int,
@@ -143,3 +187,17 @@ def add_constraints(solver: motile.Solver):
     solver.add_constraints(MaxParents(1))
     solver.add_constraints(MaxChildren(1))
     return solver
+
+
+def expand_position(data: np.ndarray, position: List, id_: int, nhood: int = 2):
+    if len(position) == 2:
+        y, x = position
+        data[y - nhood : y + nhood + 1, x - nhood : x + nhood + 1] = id_
+    elif len(position) == 3:
+        z, y, x = position
+        data[
+            z - nhood : z + nhood + 1,
+            y - nhood : y + nhood + 1,
+            x - nhood : x + nhood + 1,
+        ] = id_
+    return data
